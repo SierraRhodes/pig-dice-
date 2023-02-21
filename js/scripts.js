4641752c44653b5637341f8d99ee0064ebcae35d
lilbitsnpieces <alreadywrote@gmail.com>
2023-02-20 19:13:37 -0800

    Add constructors for player, computer, score, dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,3 @@
-// Business Logic 
-
-// Generate random numbers 1-6 
-
-// const diceRoll = Math.floor(Math.random() * 6) + 1;
-// let image;
-// switch(diceRoll){
-//  case 1: {image = 'img/die1.png'; break;}
-//  case 2: {image = 'img/die2.png'; break;}
-//  case 3: {image = 'img/die3.png'; break;}
-//  case 4: {image = 'img/die4.png'; break;}
-//  case 5: {image = 'img/die5.png'; break;}
-//  case 6: {image = 'img/die6.png'; break;}
-//  default:break;
-// }
-// document.getElementById('image').src = "path/dice/number"+rollResult+".jpg"
-// document.getElementById("image1").src = "path/dice/number"+rollResult+".jpg"
-// function rolldice(){
-//    const rollResultElem = document.getElementById("roll-resilts")
-//    const playerRandomNum = Math.floor(Math.random() * 6) + 1;
-// }
-
-
-// console.log(diceRoll)
-
 //  Business Logic
 const diceRoll = Math.floor(Math.random() * 6) + 1;
 const diceRoll2 = Math.floor(Math.random() * 6) + 1;
@@ -49,27 +24,58 @@ switch(diceRoll2){
 }
 
 //Score
+function Score() {
+  this.player = [];
+  this.computer = [];
+}
+
+//Player
+function Player() {
+  this.roll = 0;
+  this.tempScore= 0;
+  this.score = 0;
+  this.turn = 0;
+}
+
+//Computer
+function Computer() {
+  this.roll = 0;
+  this.tempScore= 0;
+  this.score = 0;
+  this.turn = 0;
+}
+
+//Checking for 1
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    switchPlayer();
+  } else {
+    this.tempscore += this.roll;
+  }
+}
+
+
+//Hold 
+
 
 
 // UI Logic
 
-
 // Winner
-function roll() {
-  if(diceRoll > diceRoll2) {
-  document.querySelector('h2').innerHTML = "The Winner is Player 1";
-} else if (diceRoll < diceRoll2) {
+Player.prototype.winner = function() {
+  if (this.player >= 100) {
+  document.querySelector('h2').innerHTML = "The Winner is Player";
+  console.log("The winner is player");
+  } else
   document.querySelector('h2').innerHTML = "The Winner is Computer";
-} else if (diceRoll === diceRoll2) {
-  document.querySelector('h2').innerHTML = "It's a Draw!";
-}
-console.log("What's going on?");
-}
+  }
+  
+  
+
 
 
 window.addEventListener("load", function(event) {
-  // const form = document.querySelector ("form");
-  //   form.onsubmit = function(event) {
         event.preventDefault();
         const imgElement = document.getElementById('dice-image');
         imgElement.src = image;
@@ -77,9 +83,12 @@ window.addEventListener("load", function(event) {
         imgElement2.src = image2; 
         document.getElementById("result").innerHTML = "You rolled " + diceRoll + "!";
         document.getElementById("result2").innerHTML = "Computer rolled " + diceRoll2 + "!";
+        console.log("event listener")
+        const resetButton = document.getElementById("reset-button");
+        resetButton.addEventListener("click", function() {
+        document.getElementById("dice-game").reset();
+        });
         const rollButton = document.getElementById('roll-button');
         rollButton.addEventListener("click", roll);
-        // const resetButton = document.getElementById('reset-button');
-        // resetButton.addEventListener("click", roll);
-    
- });
+      });
+      
