c780c5260fcf1f44ea00be852895a6780262d961
lilbitsnpieces <alreadywrote@gmail.com>
2023-02-22 16:44:14 -0800

    Add reset button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 let player = new Player();
 let computer = new Computer();
-const diceEl = document.querySelector('.dice');
 
 //Business Logic 
 
@@ -26,11 +25,6 @@ function rollDice() {
  return diceNumber;
 }
 
-// function displayDice(){
-//   rollDice(diceNumber);
-//   diceEl.src = `img/die${diceNumber}.png`;
-// }
-
 function updateScore(diceNumber, player) {
   if (diceNumber === 1) {
     player.score = 0;
@@ -42,17 +36,12 @@ function updateScore(diceNumber, player) {
 }
 
 
-//UI Logic 
 
-const displayDiceUI = function(dice) {
-  diceEl.src = `img/die${diceNumber}.png`;
-}
+//UI Logic 
 
 function roll() {
   event.preventDefault();
-  const diceNumber = rollDice();
-  // displayDiceUI(diceNumber);
-  // determine which player's turn it is
+  const diceNumber = Math.floor(Math.random() * 6) + 1;
   if (player.turn === 1) {
     updateScore(diceNumber, player);
      // updates the UI to display the updated score
@@ -60,7 +49,7 @@ function roll() {
   } else {
     updateScore(diceNumber, computer);
     document.getElementById("computer-score").innerText = computer.score;
-  }
+  }  
 }
 
 function switchPlayer() {
@@ -77,7 +66,6 @@ function switchPlayer() {
 }
 
 // Hold
-
 function hold() {
   event.preventDefault();
   if (player.turn === 1) {
@@ -97,26 +85,32 @@ function hold() {
  //Who is the winner
 function displayWinner() {
  if (player.currentScore >= 10) {
- document.getElementsByClassName("winner").innerText = "The winner is Player!";
+ document.querySelector("h3").innerText = "The winner is Player!";
  console.log("The winner is Player!");
- } else if (computer.currentScore >= 10){
+ } else if (computer.currentScore >= 10) {
  document.querySelector("h3").innerText = "The winner is Computer!";
  console.log("The winner is Computer!");
- } else {
- document.querySelector("h2").innerText = "No Winner!";
+ } 
 }
+
+//Game Reset 
+//resets the game
+function resetGame() {
+  document.getElementById("player-score").innerText = 0;
+  document.getElementById("computer-score").innerText = 0;
+  document.getElementById("player-current-score").innerText = 0;
+  document.getElementById("computer-current-score").innerText = 0;
+  document.getElementById("current-turn").innerText = "Player's Turn";
+  document.querySelector("h3").innerText = null;
 }
- 
- 
-
-
-
 
 
 
 window.addEventListener("load", function(event) {
   const holdButton = document.getElementById("hold-button");
   holdButton.addEventListener("click", hold);
+  const resetButton = document.getElementById("reset-button");
+  resetButton.addEventListener("click", resetGame);
   const rollButton = document.getElementById("roll-button");
   rollButton.addEventListener("click", roll);
  });
